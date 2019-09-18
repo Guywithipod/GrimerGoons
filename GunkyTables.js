595fc7d91e49a98dd4bef1c3adfe9acdfab08951
Anthony Dominguez <Anthonydv7@outlook.com>
2019-09-18 14:10:32 -0500

    gunkeytables added and imported to server

---
+++ GunkyTables.js
@@ -0,0 +1,39 @@
+const gunkyTables = [
+    {
+        isReserverd = false
+        ID: "",
+        Name: "",
+        Email: "",
+        Phone: "",
+    },
+    {
+        isReserverd = false
+        ID: "",
+        Name: "",
+        Email: "",
+        Phone: "",
+    },
+    {
+        isReserverd = false
+        ID: "",
+        Name: "",
+        Email: "",
+        Phone: "",
+    },
+    {
+        isReserverd = false
+        ID: "",
+        Name: "",
+        Email: "",
+        Phone: "",
+    },
+    {
+        isReserverd = false
+        ID: "",
+        Name: "",
+        Email: "",
+        Phone: "",
+    },
+];
+
+module.exports = gunkyTables;
