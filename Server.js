f9c2a5f94a513bebd8a4b3d00513e0134d90f584
Anthony Dominguez <Anthonydv7@outlook.com>
2019-09-18 14:26:32 -0500

    first node app deploy

--- Server.js
+++ Server.js
@@ -3,8 +3,9 @@ const express = require("express");
 const app = express();
 const grimeyRoutes = require("./GrimeyRoutes.js")
 
+
 grimeyRoutes(app)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-const grimeyRoutes = require("./GunkyTables.js")
+const grimeyTables = require("./GunkyTables.js")
