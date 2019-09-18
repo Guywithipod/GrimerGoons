d7c23dfa3a822e0bc6bacb5cea867d6f65bc3dfe
Anthony Dominguez <Anthonydv7@outlook.com>
2019-09-18 13:25:56 -0500

    first commit

---
+++ GrimerGoons.js
@@ -0,0 +1,23 @@
+// Stay grimey fam, major goons only
+
+const express = require("express");
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var tableReservations = [
+    {
+      ID: "",
+      Name: "me",
+      Email: "b@b.org",
+      Phone: 1,
+    },
+    {
+        ID: "#@zzqr",
+        Name: "xxx",
+        Email: "z@y.sh",
+        Phone: 123,
+    },
+
+  ];
